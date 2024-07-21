235067201cdf18f0a8da894f1d129111020b7d18
yanguojin <guojin_yjs@cumt.edu.cn>
2024-07-21 23:05:36 +0800

    Add ov_genai_generation_config_create and ov_genai_generation_config_create_with_json API.

--- src/OpenVinoGenAIExtern/include/ov_genai_generation_config.h
+++ src/OpenVinoGenAIExtern/include/ov_genai_generation_config.h
@@ -1,13 +1,52 @@
-// Copyright (C) 2024 Yan Guojin
+// Copyright (C) 2024 Yan Guojin. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 //
 
-/**
- * @brief This is a header file for the ov_genai_generation_config C API, which is a C wrapper for ov::Core class.
- * This class represents an OpenVINO runtime Core entity.
- * @file ov_genai_generation_config.h
- */
 
+/**
+* @file ov_genai_generation_config.h
+* @brief This is a header file for the ov_genai_generation_config C API, which is a C wrapper for ov::genai::GenerationConfig.
+* @version 1.0
+* @author Yan Guojin guojin_yjs@cumt.edu.cn
+* @date 2024/7/21
+*/
+
+#include "openvino/genai/generation_config.hpp"
+#include "openvino/genai/llm_pipeline.hpp"
+#include "openvino/genai/streamer_base.hpp"
+#include "openvino/genai/tokenizer.hpp"
+#include "openvino/genai/visibility.hpp"
+
+#include "ov_genai_common.h"
+
+
+/**
+ * @struct ov_genai_generation_config_t
+ * @ingroup ov_genai_generation_config_c_api
+ * @brief type define ov_genai_generation_config_t from ov_genai_generation_config
+ */
+typedef struct ov_genai_generation_config ov_genai_generation_config_t;
+
+
+/**
+ * @brief Constructs OpenVINO GenAI GenerationConfig instance by default.
+ * @ingroup ov_genai_generation_config_c_api
+ * @param core A pointer to the newly created ov_genai_generation_config_t.
+ * @return Status code of the operation: OK(0) for success.
+ */
+OPENVINO_C_API(ov_status_e)
+ov_genai_generation_config_create(ov_genai_generation_config_t** generation_config);
+
+/**
+ * @brief Constructs OpenVINO GenAI GenerationConfig instance by default.
+ * @ingroup ov_genai_generation_config_c_api
+ * @param json_path 
+ * @param core A pointer to the newly created ov_genai_generation_config_t.
+ * @return Status code of the operation: OK(0) for success.
+ */
+OPENVINO_C_API(ov_status_e)
+ov_genai_generation_config_create_with_json(const char* json_path,
+    ov_genai_generation_config_t** generation_config);
 
 
 #pragma once
--- src/OpenVinoGenAIExtern/src/ov_genai_generation_config.cpp
+++ src/OpenVinoGenAIExtern/src/ov_genai_generation_config.cpp
@@ -1,3 +1,41 @@
 // Copyright (C) 2024 Yan Guojin
 // SPDX-License-Identifier: Apache-2.0
 //
+
+/**
+* @file ov_genai_generation_config.c
+* @brief This is a header file for the ov_genai_generation_config C API, which is a C wrapper for ov::genai::GenerationConfig.
+* @version 1.0
+* @author Yan Guojin guojin_yjs@cumt.edu.cn
+* @date 2024/7/21
+*/
+
+#include "ov_genai_generation_config.h"
+#include <memory>
+
+#include "genai_common.h"
+
+
+
+ov_status_e ov_genai_generation_config_create_with_json(
+    const char* json_path,
+    ov_genai_generation_config_t** generation_config)
+{
+    if (!generation_config || !json_path) {
+        return ov_status_e::INVALID_C_PARAM;
+    }
+
+    try {
+        std::unique_ptr<ov_genai_generation_config_t> _generation_config(new ov_genai_generation_config_t);
+        _generation_config->object = std::make_shared<ov::genai::GenerationConfig>(json_path);
+        *generation_config = _generation_config.release();
+    }
+    CATCH_OV_GENAI_EXCEPTIONS
+        return ov_status_e::OK;
+}
+
+ov_status_e ov_genai_generation_config_create(
+    ov_genai_generation_config_t** generation_config){
+    return ov_genai_generation_config_create_with_json("", generation_config);
+}
+
