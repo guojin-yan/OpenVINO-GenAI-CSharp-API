7cbbcfc06466559d9bcf561751319deee296446f
yanguojin <guojin_yjs@cumt.edu.cn>
2024-07-21 12:43:04 +0800

    Add ov_genai_generation_config.cpp and ov_genai_generation_config.h file.

---
+++ src/OpenVinoGenAIExtern/include/ov_genai_generation_config.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2024 Yan Guojin
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/**
+ * @brief This is a header file for the ov_genai_generation_config C API, which is a C wrapper for ov::Core class.
+ * This class represents an OpenVINO runtime Core entity.
+ * @file ov_genai_generation_config.h
+ */
+
+
+
+#pragma once
---
+++ src/OpenVinoGenAIExtern/src/ov_genai_generation_config.cpp
@@ -0,0 +1,3 @@
+// Copyright (C) 2024 Yan Guojin
+// SPDX-License-Identifier: Apache-2.0
+//
