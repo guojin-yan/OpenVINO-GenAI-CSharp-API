9c55b8cac13c3dd371a57aa39dabf324b78c0488
yanguojin <guojin_yjs@cumt.edu.cn>
2024-07-24 22:15:23 +0800

    Add  ov_infer_request file.

---
+++ src/OpenVinoGenAIExtern/include/ov_infer_request.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 Yan Guojin. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/**
+* @file ov_infer_request.h
+* @brief This is a header file for the ov_infer_request C API, which is a C wrapper for ov::InferRequest.
+* @version 1.0
+* @author Yan Guojin guojin_yjs@cumt.edu.cn
+* @date 2024/7/24
+*/
+#pragma once
+#include "openvino/openvino.hpp"
+#include "ov_genai_common.h"
+
+
+typedef struct ov_infer_request ov_infer_request_t;
---
+++ src/OpenVinoGenAIExtern/src/ov_infer_request.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2024 Yan Guojin. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/**
+* @file ov_infer_request.h
+* @brief This is a header file for the ov_infer_request C API, which is a C wrapper for ov::InferRequest.
+* @version 1.0
+* @author Yan Guojin guojin_yjs@cumt.edu.cn
+* @date 2024/7/24
+*/
+
+#include "ov_infer_request.h"
