d039b4e04877c96926f6ec304d48da1624660385
yanguojin <guojin_yjs@cumt.edu.cn>
2024-07-31 21:44:04 +0800

    Fix related interface parameter types.

--- src/OpenVinoGenAIExtern/include/ov_genai_llm_pipeline.h
+++ src/OpenVinoGenAIExtern/include/ov_genai_llm_pipeline.h
@@ -21,6 +21,7 @@
 #include "ov_infer_request.h"
 #include "ov_tensor.h"
 #include "ov_genai_decoded_results.h"
+#include "ov_genai_encoded_results.h"
 #include "ov_genai_generation_config.h"
 #include "ov_genai_tokenizer.h"
 
@@ -32,12 +33,18 @@
  */
 typedef struct ov_genai_llm_pipeline ov_genai_llm_pipeline_t;
 
+
+
 /**
  * @struct ov_string_array
  * @ingroup ov_genai_llm_pipeline_c_api
  * @brief String array structure.
  */
-typedef struct ov_genai_char_arrays ov_genai_char_arrays_t;
+typedef struct {
+	char** string_array;
+	size_t size;
+} ov_genai_char_arrays_t;
+
 
 typedef bool(__stdcall* ov_genai_streamer_callback_t)(std::string str);
 
@@ -63,13 +70,27 @@ ov_genai_llm_pipeline_create_with_model_path(
  * @brief Constructs an LLMPipeline from already existing infer InferRequest and Tokenizer.
  * @ingroup ov_genai_llm_pipeline_c_api
  * @param request infer request of the model
+ * @param tokenizer initialized Tokenizer
+ * @param llm_pipeline A point to ov_genai_llm_pipeline_t
+ * @return Status code of the operation: OK(0) for success.
+ */
+OPENVINO_C_API(ov_status_e)
+ov_genai_llm_pipeline_create_with_request(
+	const ov_infer_request_t* request,
+	const ov_genai_tokenizer_t* tokenizer,
+	ov_genai_llm_pipeline_t** llm_pipeline);
+
+/**
+ * @brief Constructs an LLMPipeline from already existing infer InferRequest and Tokenizer.
+ * @ingroup ov_genai_llm_pipeline_c_api
+ * @param request infer request of the model
  * @param tokenizer initialized Tokenizer 
  * @param generation_config optional generation_config, be default will be initialized for greedy decoding
  * @param llm_pipeline A point to ov_genai_llm_pipeline_t
  * @return Status code of the operation: OK(0) for success.
  */
 OPENVINO_C_API(ov_status_e)
-ov_genai_llm_pipeline_create_with_request(
+ov_genai_llm_pipeline_create_with_request_config(
 	const ov_infer_request_t* request,
 	const ov_genai_tokenizer_t* tokenizer,
 	const ov_genai_generation_config_t* generation_config,
@@ -129,7 +150,7 @@ ov_genai_llm_pipeline_generate_string(
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_strings(
 	ov_genai_llm_pipeline_t* llm_pipeline,
-	const ov_genai_char_arrays_t* inputs_array,
+	const ov_genai_char_arrays_t inputs_array,
 	ov_genai_decoded_results_t** decoded_results
 );
 
@@ -162,7 +183,7 @@ ov_genai_llm_pipeline_generate_string_with_config(
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_strings_with_config(
 	ov_genai_llm_pipeline_t* llm_pipeline,
-	const ov_genai_char_arrays_t* inputs_array,
+	const ov_genai_char_arrays_t inputs_array,
 	const ov_genai_generation_config_t* generation_config,
 	ov_genai_decoded_results_t** decoded_results
 );
@@ -196,7 +217,7 @@ ov_genai_llm_pipeline_generate_string_with_function(
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_strings_with_function(
 	ov_genai_llm_pipeline_t* llm_pipeline,
-	const ov_genai_char_arrays_t* inputs_array,
+	const ov_genai_char_arrays_t inputs_array,
 	ov_genai_streamer_callback_t* callback,
 	ov_genai_decoded_results_t** decoded_results
 );
@@ -233,7 +254,7 @@ ov_genai_llm_pipeline_generate_string_with_config_function(
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_strings_with_config_function(
 	ov_genai_llm_pipeline_t* llm_pipeline,
-	const ov_genai_char_arrays_t* inputs_array,
+	const ov_genai_char_arrays_t inputs_array,
 	const ov_genai_generation_config_t* generation_config,
 	ov_genai_streamer_callback_t* callback,
 	ov_genai_decoded_results_t** decoded_results
@@ -252,7 +273,7 @@ OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_string_with_config_map(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const char* inputs,
-	const generation_config_param_t* config_param,
+	const generation_config_param_t config_param,
 	ov_genai_decoded_results_t** decoded_results
 );
 
@@ -268,8 +289,8 @@ ov_genai_llm_pipeline_generate_string_with_config_map(
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_strings_with_config_map(
 	ov_genai_llm_pipeline_t* llm_pipeline,
-	const ov_genai_char_arrays_t* inputs_array,
-	const generation_config_param_t* config_param,
+	const ov_genai_char_arrays_t inputs_array,
+	const generation_config_param_t config_param,
 	ov_genai_decoded_results_t** decoded_results
 );
 
@@ -278,28 +299,28 @@ ov_genai_llm_pipeline_generate_strings_with_config_map(
  * @brief Low level generate to be called with already encoded input_ids tokens.
  * @param llm_pipeline A point to ov_genai_llm_pipeline_t.
  * @param tensor The input_ids encoded input prompt tokens.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_tensor(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const ov_tensor_t* tensor,
-	ov_genai_decoded_results_t** decoded_results
+	ov_genai_encoded_results_t** encoded_results
 );
 
 /**
  * @brief Low level generate to be called with already encoded input_ids tokens.
  * @param llm_pipeline A point to ov_genai_llm_pipeline_t.
  * @param tokenized_inputs The pair of (input_ids, attentino_mask) encoded input prompt tokens.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_tokenized_inputs(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const ov_genai_tokenized_inputs_t* tokenized_inputs,
-	ov_genai_decoded_results_t** decoded_results
+	ov_genai_encoded_results_t** encoded_results
 );
 
 
@@ -308,7 +329,7 @@ ov_genai_llm_pipeline_generate_tokenized_inputs(
  * @param llm_pipeline A point to ov_genai_llm_pipeline_t.
  * @param tensor The input_ids encoded input prompt tokens.
  * @param generation_config Class to keep generation config parameters.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
@@ -316,7 +337,7 @@ ov_genai_llm_pipeline_generate_tensor_with_config(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const ov_tensor_t* tensor,
 	const ov_genai_generation_config_t* generation_config,
-	ov_genai_decoded_results_t** decoded_results
+	ov_genai_encoded_results_t** encoded_results
 );
 
 /**
@@ -332,7 +353,7 @@ ov_genai_llm_pipeline_generate_tokenized_inputs_with_config(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const ov_genai_tokenized_inputs_t* tokenized_inputs,
 	const ov_genai_generation_config_t* generation_config,
-	ov_genai_decoded_results_t** decoded_results
+	ov_genai_encoded_results_t** decoded_results
 );
 
 
@@ -341,7 +362,7 @@ ov_genai_llm_pipeline_generate_tokenized_inputs_with_config(
  * @param llm_pipeline A point to ov_genai_llm_pipeline_t.
  * @param tensor The input_ids encoded input prompt tokens.
  * @param callback Delegate function pointer.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
@@ -349,7 +370,7 @@ ov_genai_llm_pipeline_generate_tensor_with_function(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const ov_tensor_t* tensor,
 	ov_genai_streamer_callback_t* callback,
-	ov_genai_decoded_results_t** decoded_results
+	ov_genai_encoded_results_t** encoded_results
 );
 
 /**
@@ -357,7 +378,7 @@ ov_genai_llm_pipeline_generate_tensor_with_function(
  * @param llm_pipeline A point to ov_genai_llm_pipeline_t.
  * @param tokenized_inputs The pair of (input_ids, attentino_mask) encoded input prompt tokens.
  * @param callback Delegate function pointer.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
@@ -365,7 +386,7 @@ ov_genai_llm_pipeline_generate_tokenized_inputs_with_function(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const ov_genai_tokenized_inputs_t* tokenized_inputs,
 	ov_genai_streamer_callback_t* callback,
-	ov_genai_decoded_results_t** decoded_results
+	ov_genai_encoded_results_t** encoded_results
 );
 
 
@@ -375,7 +396,7 @@ ov_genai_llm_pipeline_generate_tokenized_inputs_with_function(
  * @param tensor The input_ids encoded input prompt tokens.
  * @param generation_config Class to keep generation config parameters.
  * @param callback Delegate function pointer.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
@@ -384,7 +405,7 @@ ov_genai_llm_pipeline_generate_tensor_with_config_function(
 	const ov_tensor_t* tensor,
 	const ov_genai_generation_config_t* generation_config,
 	ov_genai_streamer_callback_t* callback,
-	ov_genai_decoded_results_t** decoded_results
+	ov_genai_encoded_results_t** encoded_results
 );
 
 /**
@@ -393,7 +414,7 @@ ov_genai_llm_pipeline_generate_tensor_with_config_function(
  * @param tokenized_inputs The pair of (input_ids, attentino_mask) encoded input prompt tokens.
  * @param generation_config Class to keep generation config parameters.
  * @param callback Delegate function pointer.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
@@ -402,7 +423,7 @@ ov_genai_llm_pipeline_generate_tokenized_inputs_with_config_function(
 	const ov_genai_tokenized_inputs_t* tokenized_inputs,
 	const ov_genai_generation_config_t* generation_config,
 	ov_genai_streamer_callback_t* callback,
-	ov_genai_decoded_results_t** decoded_results
+	ov_genai_encoded_results_t** encoded_results
 );
 
 
@@ -411,15 +432,15 @@ ov_genai_llm_pipeline_generate_tokenized_inputs_with_config_function(
  * @param llm_pipeline A point to ov_genai_llm_pipeline_t.
  * @param tensor The input_ids encoded input prompt tokens.
  * @param config_param Structure to keep generation config parameters.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_tensor_with_config_map(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const ov_tensor_t* tensor,
-	const generation_config_param_t* config_param,
-	ov_genai_decoded_results_t** decoded_results
+	const generation_config_param_t config_param,
+	ov_genai_decoded_results_t** encoded_results
 );
 
 /**
@@ -427,15 +448,15 @@ ov_genai_llm_pipeline_generate_tensor_with_config_map(
  * @param llm_pipeline A point to ov_genai_llm_pipeline_t.
  * @param tokenized_inputs The pair of (input_ids, attentino_mask) encoded input prompt tokens.
  * @param config_param Structure to keep generation config parameters.
- * @param decoded_results EncodedResults a structure with resulting tokens and scores
+ * @param encoded_results EncodedResults a structure with resulting tokens and scores
  * @return Status code of the operation: OK(0) for success.
 */
 OPENVINO_C_API(ov_status_e)
 ov_genai_llm_pipeline_generate_tokenized_inputs_with_config_map(
 	ov_genai_llm_pipeline_t* llm_pipeline,
 	const ov_genai_tokenized_inputs_t* tokenized_inputs,
-	const generation_config_param_t* config_param,
-	ov_genai_decoded_results_t** decoded_results
+	const generation_config_param_t config_param,
+	ov_genai_encoded_results_t** encoded_results
 );
 
 
