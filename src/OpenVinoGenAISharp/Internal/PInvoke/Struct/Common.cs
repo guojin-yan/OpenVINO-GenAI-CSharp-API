d3835742acd0c0e87c82c42196cae57defaca37c
yanguojin <guojin_yjs@cumt.edu.cn>
2024-10-13 22:14:45 +0800

    Add StructCommon.StringArrayToStruct API.

---
+++ src/OpenVinoGenAISharp/Internal/PInvoke/Struct/Common.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenVinoSharp.GenAI.Internal
+{
+    public static class StructCommon
+    {
+
+        public static StringArray StringArrayToStruct(string[] strs)
+        {
+            StringArray stringArray = new StringArray();
+            ulong size = (ulong)strs.Length;
+            IntPtr[] str_ptrs = new IntPtr[strs.Length];
+            for (int i = 0; i < strs.Length; ++i)
+            {
+                str_ptrs[i] = Marshal.StringToHGlobalAnsi(strs[i]);
+            }
+
+            IntPtr data = Marshal.AllocHGlobal(IntPtr.Size * strs.Length);
+            Marshal.Copy(str_ptrs, 0, data, strs.Length);
+            return new StringArray { Data = data, Size = size };
+        }
+    }
+}
